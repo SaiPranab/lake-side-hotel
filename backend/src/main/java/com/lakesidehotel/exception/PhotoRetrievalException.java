f183708802bdd510575df0c64b475ad303979b21
Sai <saipranabpatra5233@gmail.com>
2024-10-07 11:17:56 +0530

    project completed

---
+++ backend/src/main/java/com/lakesidehotel/exception/PhotoRetrievalException.java
@@ -0,0 +1,8 @@
+package com.lakesidehotel.exception;
+
+
+public class PhotoRetrievalException extends RuntimeException {
+    public PhotoRetrievalException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/lakesidehotel/exception/RoleAlreadyExistException.java
@@ -0,0 +1,8 @@
+package com.lakesidehotel.exception;
+
+
+public class RoleAlreadyExistException extends RuntimeException {
+    public RoleAlreadyExistException(String message) {
+        super(message);
+    }
+}
